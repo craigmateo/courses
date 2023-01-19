8cc96454e69c1d76eab54029f9fbd10eb5e5b222
craigmateo <craigfrayne@gmail.com>
2023-01-19 13:56:09 +0100

    added project

---
+++ PROGRAMMING/GUIDED_PROJECTS/automobile-listing-website/index.php
@@ -0,0 +1,85 @@
+<!doctype html>
+
+<!-- Web page "Hello World Autos"       -->
+<!-- Created by Harrison Kong           -->
+<!-- Copyright (C) Coursera 2021        -->
+
+<html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv=ÂPragmaÂ content=Âno-cacheÂ>
+<meta http-equiv=ÂExpiresÂ content=Â-1?>
+<meta http-equiv=ÂCACHE-CONTROLÂ content=ÂNO-CACHEÂ>
+<!-- CSS Stylesheets -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link rel="stylesheet" href="css/index.css">
+
+<title>Hello World Autos</title>   
+
+</head>
+
+<body>
+
+<?php 
+
+    $vehicleMake = "Astro";
+    $vehicleModel = "Estrella";
+    $vehicleYear = 2021;
+    $vehicleMileage = 5000;
+    $vehiclePrice = 50000.00;
+    $vehicleImage = "veh-01.jpg";
+
+    $vehicles = [
+        [ "Astro", "Estrella", "2021", 500, 50000, "veh-01.jpg" ],
+        [ "Terraza", "Spinneo", "2020", 30000, 31000, "veh-02.jpg" ],
+        [ "Sage", "Escotar", "2014", 70000, 15000, "veh-03.jpg" ]
+    ];
+
+?>
+
+<div class='outer-div'>
+<h1><?php echo "Hello World Autos" ?></h1>
+<p class="slogan">Where everyone can afford to buy a vehicle!</p>
+
+<table class="vehicle-table" id="vehicle-table">
+    <colgroup>
+        <col width="20%"><col width="80%">
+    </colgroup>
+    <tbody>
+        <!-- rows of vehicles -->
+
+        <?php foreach($vehicles as $vehicle) {
+            $price = number_format($vehicle[4], 2);
+            echo <<<FOREACHVEHICLE
+            <tr>
+                <td class="top-aligned">
+                    <img class="thumbnail" src="images/$vehicle[5]" alt="vehicle picture">
+                </td>
+                <td>
+                    <p class="vehicle-make">$vehicle[0]</p>
+                    <p class="vehicle-model">$vehicle[1]</p>
+                    <hr class="vehicle-hr">
+                    <p class="right-aligned"><span class="data-label">Year: </span><span class="vehicle-year">$vehicle[2]</span>
+                    &nbsp;&nbsp;<span class="data-label">Mileage: </span><span class="vehicle-mileage">$vehicle[3]</span></p>
+                    <p class="vehicle-price right-aligned">$$price</p>
+                    <hr />
+                </td>
+            </tr>
+            FOREACHVEHICLE;
+        };
+        ?>
+
+    </tbody>
+
+</table>
+
+</div>
+
+</body>
+<footer>
+    <?php include "footer.php"?>
+</footer>
+</html>
---
+++ PROGRAMMING/GUIDED_PROJECTS/automobile-listing-website/payment-plan.php
@@ -0,0 +1,49 @@
+<!doctype html>
+
+<!-- Web page "Hello World Autos"       -->
+<!-- Created by Harrison Kong           -->
+<!-- Copyright (C) Coursera 2021        -->
+
+<html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv=ÂPragmaÂ content=Âno-cacheÂ>
+<meta http-equiv=ÂExpiresÂ content=Â-1?>
+<meta http-equiv=ÂCACHE-CONTROLÂ content=ÂNO-CACHEÂ>
+<!-- CSS Stylesheets -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link rel="stylesheet" href="css/payment-plan.css">
+
+<title>Hello World Autos</title>   
+
+</head>
+
+<body>
+
+    <h1>Your Personalised Payment Plan</h1>
+
+    <div class="content-area">
+        <img class="hero" src="images/img.jpg ?>" alt="vehicle image" />
+        <p class="vehicle-make">Make</p>
+        <p class="vehicle-model">Model</p>
+        <hr class="vehicle-hr">
+        <p class="vehicle-price">$Price</p>
+        <p><span class="data-label">Repayment duration: </span><span class="data-item">Repayment Duration months</span></p>
+        <p><span class="data-label">Interest rate: </span><span class="data-item">Interest Rate% APR</span></p>
+        <p><span class="data-label">Total payment: </span><span class="data-item">$Total Payment</span></p>
+        <p><span class="data-label">Total interest: </span><span class="data-item">$Total Interest</span></p>
+        <hr class="short-line">
+        <p><span class="data-label">Monthly payment: </span><span class="focal-point">$Monthly Payment</span></p>
+        <br /><br />
+    </div>
+
+</body>
+
+<footer>
+    <?php include "footer.php" ?>
+</footer>
+
+</html>
